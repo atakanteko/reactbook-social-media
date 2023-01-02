aa4b48aa42316a4b5eb82872f6197d402d255bb7
Atakan Tekoğlu <atakantekoglu@gmail.com>
2023-01-02 16:45:07 +0300

    crate profile page and complete banner section

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
+import Profile from './pages/profile/Profile';
 
 type Props = {}
 
 const App = (props: Props) => {
   return (
     <main>
-      <Login />
+      <Profile />
     </main>
   )
 }
---
+++ src/pages/profile/Profile.tsx
@@ -0,0 +1,48 @@
+import Container from '@/components/Container'
+import Feeds from '@/components/feeds/Feeds'
+import Rightbar from '@/components/rightbar/Rightbar'
+import Sidebar from '@/components/sidebar/Sidebar'
+import Topbar from '@/components/topbar/Topbar'
+import UserImg from '@/assets/images/person/7.jpeg'
+import CoverImg from '@/assets/images/post/3.jpeg'
+
+type Props = {}
+
+const Profile = (props: Props) => {
+    return (
+        <div>
+            <Topbar />
+            <Container>
+                <Sidebar />
+                <div className="profile">
+                    <div className="profileRight">
+                        <div className="profileRightTop">
+                            <div className="profileCover">
+                                <img 
+                                    src={CoverImg} 
+                                    alt="cover image" 
+                                    className="profileCoverImg"
+                                />
+                                <img 
+                                    src={UserImg} 
+                                    alt="user image" 
+                                    className="profileUserImg"
+                                />
+                            </div>
+                            <div className="profileInfo">
+                                <h4 className="profileInfoName">Atakan TEKO</h4>
+                                <span className="profileInfoDesc">Hello there!</span>
+                            </div>
+                        </div>
+                        <div className="profileRightBottom">
+                            <Feeds />
+                            <Rightbar />
+                        </div>
+                    </div>
+                </div>  
+            </Container>
+        </div>
+      )
+}
+
+export default Profile
